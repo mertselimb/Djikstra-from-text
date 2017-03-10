c463eeafd2d7b512ce1f048568f97a20f8eb81c3
mertselimb <mertselimb@gmail.com>
2017-03-10 20:40:57 +0300

    starters

---
+++ kod/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int j ,i ,sehirler[22][22] ;
+    for(i=0 ; i<22; i++){
+            for( j=0;j<22;j++){
+                sehirler[i][j]=99800;
+            }
+    }
+
+    return 0;
+}
